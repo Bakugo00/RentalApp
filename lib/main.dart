5068391c0499f1a8ade4f31bff55ea0f6e17ff88
Bakugo00 <haaba05@gmail.com>
2023-12-19 18:16:32 +0100

    adding project

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'exports.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ThemeProvider>(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider<RoleProvider>(create: (_) => RoleProvider()),
+        ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
+        ChangeNotifierProvider<UserProvider>(create: (_) => UserProvider()),
+        ChangeNotifierProvider<ChatProvider>(create: (_) => ChatProvider()),
+        ChangeNotifierProvider<ReviewProvider>(create: (_) => ReviewProvider()),
+        ChangeNotifierProvider<RentProvider>(create: (_) => RentProvider()),
+        ChangeNotifierProvider<PropertyProvider>(
+            create: (_) => PropertyProvider()),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, theme, child) {
+          return MaterialApp(
+            title: 'Rentatouille',
+            debugShowCheckedModeBanner: false,
+            themeMode: theme.themeMode,
+            theme: ThemeData(
+              textTheme: TextTheme(
+                headlineLarge: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 26,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.lightThemeSecondaryColor),
+                headlineMedium: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 22,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.lightThemeSecondaryColor),
+                headlineSmall: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 20,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.lightThemeSecondaryColor),
+                bodyLarge: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 18,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.lightThemeSecondaryColor),
+                bodyMedium: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 16,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.lightThemeSecondaryColor),
+                bodySmall: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 14,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.lightThemeSecondaryColor),
+              ),
+              useMaterial3: true,
+              scaffoldBackgroundColor: AppColors.lightThemePrimaryColor,
+              primaryColor: AppColors.lightThemeSecondaryColor,
+            ),
+            darkTheme: ThemeData(
+              textTheme: TextTheme(
+                headlineLarge: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 26,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.darkThemeSecondaryColor),
+                headlineMedium: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 22,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.darkThemeSecondaryColor),
+                headlineSmall: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 20,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.darkThemeSecondaryColor),
+                bodyLarge: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 18,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.darkThemeSecondaryColor),
+                bodyMedium: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 16,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.darkThemeSecondaryColor),
+                bodySmall: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    fontSize: Responsive.screenHeight(context) * 0.001 * 14,
+                    height: Responsive.screenHeight(context) * 0.0018,
+                    color: AppColors.darkThemeSecondaryColor),
+              ),
+              useMaterial3: true,
+              scaffoldBackgroundColor: AppColors.darkThemePrimaryColor,
+              primaryColor: AppColors.darkThemeSecondaryColor,
+            ),
+            home: const SplashScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
